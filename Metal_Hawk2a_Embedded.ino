29c4e28bf04365417b00c50fd16fd2ee0bfc9037
ebner62 <ebner62.tyler@gmail.com>
2026-02-10 20:27:04 -0600

    Added the ino, bno and also started on the telemetry. Remeber the xbee is on Serial8

--- Metal_Hawk2a_Embedded.ino
+++ Metal_Hawk2a_Embedded.ino
@@ -2,7 +2,6 @@
 #include <Arduino.h>
 #include <SPI.h> //idk if we need this
 #include <Adafruit_Sensor.h> //BMP581
-#include <SoftwareSerial.h> //XBEE
 #include "Adafruit_BMP5xx.h" //BMP581
 #include <Adafruit_INA260.h> //INA260
 #include <SparkFun_u-blox_GNSS_v3.h> // SAM-M10Q
@@ -12,18 +11,22 @@
 //======
 // Pins
 //======
-//const uint8_t XBee_RX = 35; // idk what pins are connected
-//const uint8_t XBee_TX = 34; // idk what pins are connected
+
+#define BNO08X_INT 19
+#define BNO08X_RESET -1
 
 //=========
 // Sensors
 //=========
-//SoftwareSerial XBee(XBee_TX, XBee_RX); //XBEE use <Serial8.begin(9600);
 Adafruit_BMP5xx bmp581; //Declaring BMP581
 bmp5xx_powermode_t desiredMode = BMP5XX_POWERMODE_NORMAL; // Cache desired power mode
 SFE_UBLOX_GNSS sam_m10q; //Declaring SAM-M10Q
 Adafruit_Sensor *bmp_temp = NULL;
 Adafruit_Sensor *bmp_pressure = NULL;
+Adafruit_BNO08x  bno085(BNO08X_RESET);
+sh2_SensorValue_t sensorValue; //bno idk what we use it for
+Adafruit_INA260 ina260 = Adafruit_INA260();
+
 
 //=========
 // Servo's
@@ -51,46 +54,42 @@ const uint8_t release_s_pin = 9; //idk what servo pin yet
 const uint8_t port_s_pin = 9; //idk what servo pin yet
 const uint8_t starboard_s_pin = 9; //idk what servo pin yet
 
-//========
-// states
-//========
-String sw_states[7] = {"LAUNCH_PAD", "ASCENT", "APOGEE", "DESCENT", "PROBE_RELEASE", "PAYLOAD_RELEASE", "LANDED"};
-String sw_state = sw_states[0];
-
 //========================
 // telemetry declarations
 //========================
-int TEAM_ID = 1094;
-//MISSION_TIME idk how to do this just yet hh:mm:ss
-int PACKET_COUNT = 0;
-//char MODE = "F"; //F or S
-//STATE has already been decleared
-//ALLTITUDE will probe be a double but has a resolution of 0.1 meters
-double TEMPERATURE;
-double PRESSURE;
-double VOLTAGE;
-double CURRENT;
-int GYRO_R;
-int GYRO_P;
-int GYRO_Y;
-int ACCEL_R;
-int ACCEL_P;
-int ACCEL_Y;
-String GPS_TIME;
-long GPS_ALLTITUDE;
-long GPS_LATITUDE;
-long GPS_LONGITUDE;
-byte GPS_SATS;
+String TELEMETRY;
+int TEAM_ID = 1094; // Done
+String MISSION_TIME; // Done it is = to GPS_TIME
+int PACKET_COUNT = 0; // Done
+char MODE = 'F'; //F or S
+String sw_state = "LAUNCH_PAD"; // Done
+double ALTITUDE; // Done
+double TEMPERATURE; // Done
+double PRESSURE; // Done
+float VOLTAGE; // Done
+float CURRENT; // Done
+float GYRO_R; // Done
+float GYRO_P; // Done
+float GYRO_Y; // Done
+float ACCEL_R; // Done
+float ACCEL_P; // Done
+float ACCEL_Y; // Done
+String GPS_TIME; // Done
+long GPS_ALLTITUDE; // Done
+long GPS_LATITUDE; // Done
+long GPS_LONGITUDE; // Done
+byte GPS_SATS; // Done
 String CMD_ECHO;
 double STRB_ANGLE;
 double PORT_ANGLE;
 double VECTOR_PRODUCT;
 
-// IDK what this does but i think we need it
+
 long last_time = 0; //Simple local timer. Limits amount if I2C traffic to u-blox module.
+float lastVelR = 0, lastVelP = 0, lastVelY = 0;
+unsigned long bno_last_micros = 0;
 
 //temporary
-    double ALTITUDE = 0;
     double VELOCITY = 0;
     double peak_altitude = 0;
     double release = 100000000;
@@ -102,34 +101,82 @@ void heading_error() {
 }
 
 void collect_telemetry() {
+if (bno085.getSensorEvent(&sensorValue)){
+  if (sensorValue.sensorId == SH2_GYROSCOPE_CALIBRATED) {
+    unsigned long currentMicros = micros();
+    float dt = (currentMicros - bno_last_micros) / 1000000.0;
 
-if (millis() - last_time > 1000) // Waits 1 second 
-{
+    if (dt > 0) {
+      // Get current velocity in deg/s
+      float currVelP = sensorValue.un.gyroscope.x * 57.2958;
+      float currVelR = sensorValue.un.gyroscope.y * 57.2958;
+      float currVelY = sensorValue.un.gyroscope.z * 57.2958;
+
+      // Calculate ACCEL (deg/s^2)
+      ACCEL_P = (currVelP - lastVelP) / dt;
+      ACCEL_R = (currVelR - lastVelR) / dt;
+      ACCEL_Y = (currVelY - lastVelY) / dt;
+
+      // Store current for next calculation
+      lastVelP = currVelP;
+      lastVelR = currVelR;
+      lastVelY = currVelY;
+      bno_last_micros = currentMicros;
+
+      // Update your Telemetry Gyro vars (current velocity)
+      GYRO_P = currVelP;
+      GYRO_R = currVelR;
+      GYRO_Y = currVelY;
+    }
+  }
+}
+if (millis() - last_time > 1000) {// Waits 1 second
+  last_time = millis();
   //==========
   // SAM-M10Q
   //==========
+
   GPS_LATITUDE = sam_m10q.getLatitude();
   GPS_LONGITUDE = sam_m10q.getLongitude();
   GPS_ALLTITUDE = sam_m10q.getAltitude();
   GPS_SATS = sam_m10q.getSIV();
-  GPS_TIME = String(sam_m10q.getHour()) + ":" + String(sam_m10q.getMinute()) +  ":" + String(sam_m10q.getSecond());
+  char time_buffer[15];
+  sprintf(time_buffer, "%02d:%02d:%02d", sam_m10q.getHour(), sam_m10q.getMinute(), sam_m10q.getSecond());
+  GPS_TIME = String(time_buffer);
+  MISSION_TIME = String(time_buffer);
 
   //========
   // BMP581
   //========
+
   sensors_event_t temp_event, pressure_event;
+  bmp_temp->getEvent(&temp_event);
+  bmp_pressure->getEvent(&pressure_event);
+
   TEMPERATURE = temp_event.temperature;
-  PRESSURE = pressure_event.timestamp;
+  PRESSURE = pressure_event.pressure;
+
+  ALTITUDE = 44330 * (1.0 - pow(PRESSURE / SEALEVELPRESSURE_HPA, 0.1903));
 
   //========
-  // BNO085
+  // INA260
   //========
+
+  CURRENT = ina260.readCurrent();
+  VOLTAGE = ina260.readBusVoltage();
+
+  }
 }
-}
+
 
 void send_telemetry() {
-
-
+  if (millis() - last_time > 1000) {// Waits 1 second
+    char tel_buffer[55]; //I need to change buffer amount
+    sprintf(tel_buffer, "%d,%s,%d", TEAM_ID, MISSION_TIME, PACKET_COUNT = 0)
+    TELEMETRY = String(tel_buffer);
+    Serial8.print(TELEMETRY);
+    PACKET_COUNT += 1;
+  }
 }
 
 void recive_command(){
@@ -143,79 +190,102 @@ void pro_restart() {
 }
 
 void setup() {
-  Serial.begin(9600);
-  Wire.begin();
+  Serial8.begin(9600);
+  Wire1.begin();
   release_s.attach(release_s_pin);
   port_s.attach(port_s_pin);
   starboard_s.attach(starboard_s_pin);
 
+  //========
   // BMP581
+  //========
   bmp_temp = bmp581.getTemperatureSensor();
   bmp_pressure = bmp581.getPressureSensor();
 
   bmp581.setTemperatureOversampling(BMP5XX_OVERSAMPLING_2X);
   bmp581.setPressureOversampling(BMP5XX_OVERSAMPLING_16X);
   bmp581.setIIRFilterCoeff(BMP5XX_IIR_FILTER_COEFF_3);
-  bmp581.setOutputDataRate(BMP5XX_ODR_1_HZ);
+  bmp581.setOutputDataRate(BMP5XX_ODR_15_HZ);
   bmp581.setPowerMode(BMP5XX_POWERMODE_NORMAL);
 
-  //Print to terminal to show connection
+  //======================================
+  // Print to terminal to show connection
+  //======================================
   Serial.println("Intitalizing Metal_Hawk2a");
 
+  //=====
   // GPS
+  //=====
   sam_m10q.setI2COutput(COM_TYPE_UBX);
-  
 
-  //-----------
+  //========
+  // BNO085
+  //========
+  //bno085.enableReport(SH2_ROTATION_VECTOR);
+  //bno085.enableReport(SH2_ACCELEROMETER);
+  bno085.enableReport(SH2_GYROSCOPE_CALIBRATED);
+  bno_last_micros = micros();
+
+  //===========
   // Debugging
-  //-----------
+  //===========
   if (sam_m10q.begin() == false)
    {
-    Serial.println("SAM-M10Q Has not connected. Check wires Hayden or Rudra");
+    Serial8.println("SAM-M10Q Has not connected. Check wires Hayden or Rudra");
     while (1);
    }
-  if (!bmp581.begin(BMP5XX_ALTERNATIVE_ADDRESS, &Wire)) {
-    Serial.println("The BMP581 is not being detected,. Check wires Hayden or Rudra");
+  if (!bmp581.begin(BMP5XX_ALTERNATIVE_ADDRESS, &Wire1)) {
+    Serial8.println("The BMP581 is not being detected,. Check wires Hayden or Rudra");
     while (1);
   }
+  if (!bno085.begin_I2C(BNO08x_I2CADDR_DEFAULT, &Wire1, BNO08X_INT)) { // Added &Wire1
+    Serial8.println("BNO085 not found on Wire1");
+  while (1);
+  }
+  if (!ina260.begin()) {
+    Serial8.println("Rudra ai the ina260 is not detected");
+    while (1);
+  }
+ 
 }
 
 
 void loop() {
+  
   if(sw_state == "LAUNCH_PAD"){
     if(ALTITUDE >= 5 && VELOCITY >=5){
-      sw_state = sw_states[1];
+      sw_state = "ASCENT";
     }
   }
 
   else if(sw_state == "ASCENT"){
     if(VELOCITY <=0){     //this might not catch it
-      sw_state = sw_states[2];
+      sw_state = "APOGEE";
     }
   }
 
   else if(sw_state == "APOGEE"){
     peak_altitude = ALTITUDE;
     release = peak_altitude * 0.8;
-    sw_state = sw_states[3];
+    sw_state = "DESCENT";
   }
 
   else if(sw_state == "DESCENT"){
-    if(ALTITUDE == release){
-      sw_state = sw_states[4];
+    if(ALTITUDE <= release){
+      sw_state = "PROBE_RELEASE";
     }
   }
 
   else if(sw_state == "PROBE_RELEASE"){
     if(ALTITUDE <= 2){
       //release prob ofc
-      sw_state = sw_states[5];
+      sw_state = "PAYLOAD_RELEASE";
     }
   }
 
   else if(sw_state == "PAYLOAD_RELEASE"){
     if(VELOCITY <= 0.2){
-      sw_state = sw_states[6];
+      sw_state = "LANDED";
     }
   }
 
