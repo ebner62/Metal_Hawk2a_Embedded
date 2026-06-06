0623d5af621a236e2054961efbd1a0c6e221406c
Rudra <rudrapatel200707@gmail.com>
2026-06-05 20:36:02 -0400

    Update Metal_Hawk2a_Embedded.ino

--- Metal_Hawk2a_Embedded.ino
+++ Metal_Hawk2a_Embedded.ino
@@ -429,7 +429,7 @@ void loop() {
 
     // Curve generator
     if (!curve_generated && gps_online) {
-      p0 = gps_to_meters(lat_rad, lon_rad, TARGET_LAT_RAD, TARGET_LON_RAD);
+      p0 = gps_to_meters(lat_rad, lon_rad, TARGET_LAT_RAD, TARGET_LON_RAD); // This point should be (0,0)
       
       // Dynamic Gate Selection: Choose the gate closest to our release point
       double d1 = sqrt(pow(p0.x - gate1_m.x, 2) + pow(p0.y - gate1_m.y, 2));
